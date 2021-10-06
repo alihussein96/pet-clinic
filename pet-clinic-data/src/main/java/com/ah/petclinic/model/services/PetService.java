8984b8bde33de27d790214a84a81c7c4df153263
alihussein96 <Thesims@123>
2021-10-06 18:54:17 -0400

    create services. closes issue #5

---
+++ pet-clinic-data/src/main/java/com/ah/petclinic/model/services/PetService.java
@@ -0,0 +1,13 @@
+package com.ah.petclinic.model.services;
+
+import com.ah.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/ah/petclinic/model/services/VetService.java
@@ -0,0 +1,13 @@
+package com.ah.petclinic.model.services;
+
+import com.ah.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
